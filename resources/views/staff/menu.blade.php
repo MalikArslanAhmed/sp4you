c739ca78c2f82140970056da284dd3d60f9728af
Arslan Ahmed <malikzarslan44@gmail.com>
2023-03-28 20:26:12 +0500

    removed extra code and files

--- resources/views/staff/menu.blade.php
+++ resources/views/staff/menu.blade.php
@@ -39,16 +39,6 @@
                             </a>
                         </li>
                     @endcan
-                    @can('crm_note_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.crm-notes.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/crm-notes") || request()->is("staff/crm-notes/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-sticky-note c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.crmNote.title') }}
-                            </a>
-                        </li>
-                    @endcan
                     @can('appointment_access')
                         <li class="c-sidebar-nav-item">
                             <a href="{{ route("staff.appointments.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/appointments") || request()->is("staff/appointments/*") ? "c-active" : "" }}">
@@ -56,36 +46,6 @@
 
                                 </i>
                                 {{ trans('cruds.appointment.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('crm_document_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.crm-documents.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/crm-documents") || request()->is("staff/crm-documents/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-folder c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.crmDocument.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('photo_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.photos.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/photos") || request()->is("staff/photos/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-camera c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.photo.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('expense_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.expenses.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/expenses") || request()->is("staff/expenses/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-dollar-sign c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.expense.title') }}
                             </a>
                         </li>
                     @endcan
@@ -101,16 +61,6 @@
                     {{ trans('cruds.userManagement.title') }}
                 </a>
                 <ul class="c-sidebar-nav-dropdown-items">
-                    @can('user_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.users.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/users") || request()->is("staff/users/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-user c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.user.title') }}
-                            </a>
-                        </li>
-                    @endcan
                     @can('leave_application_access')
                         <li class="c-sidebar-nav-item">
                             <a href="{{ route("staff.leave-applications.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/leave-applications") || request()->is("staff/leave-applications/*") ? "c-active" : "" }}">
@@ -118,16 +68,6 @@
 
                                 </i>
                                 {{ trans('cruds.leaveApplication.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('user_alert_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.user-alerts.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/user-alerts") || request()->is("staff/user-alerts/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-bell c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.userAlert.title') }}
                             </a>
                         </li>
                     @endcan
@@ -163,16 +103,6 @@
                             </a>
                         </li>
                     @endcan
-                    @can('appoimntment_status_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.appoimntment-statuses.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/appoimntment-statuses") || request()->is("staff/appoimntment-statuses/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-bars c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.appoimntmentStatus.title') }}
-                            </a>
-                        </li>
-                    @endcan
                     @can('client_tag_access')
                         <li class="c-sidebar-nav-item">
                             <a href="{{ route("staff.client-tags.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/client-tags") || request()->is("staff/client-tags/*") ? "c-active" : "" }}">
@@ -181,90 +111,6 @@
                                 </i>
                                 {{ trans('cruds.clientTag.title') }}
                             </a>
-                        </li>
-                    @endcan
-                </ul>
-            </li>
-        @endcan
-        @can('administration_access')
-            <li class="c-sidebar-nav-dropdown {{ request()->is("staff/permissions*") ? "c-show" : "" }} {{ request()->is("staff/roles*") ? "c-show" : "" }} {{ request()->is("staff/audit-logs*") ? "c-show" : "" }} {{ request()->is("staff/leave-approvals*") ? "c-show" : "" }} {{ request()->is("staff/*") ? "c-show" : "" }}">
-                <a class="c-sidebar-nav-dropdown-toggle" href="#">
-                    <i class="fa-fw fas fa-toolbox c-sidebar-nav-icon">
-
-                    </i>
-                    {{ trans('cruds.administration.title') }}
-                </a>
-                <ul class="c-sidebar-nav-dropdown-items">
-                    @can('permission_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.permissions.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/permissions") || request()->is("staff/permissions/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-unlock-alt c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.permission.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('role_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.roles.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/roles") || request()->is("staff/roles/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-briefcase c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.role.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('audit_log_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.audit-logs.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/audit-logs") || request()->is("staff/audit-logs/*") ? "c-active" : "" }}">
-                                <i class="fa-fw fas fa-file-alt c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.auditLog.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('leave_approval_access')
-                        <li class="c-sidebar-nav-item">
-                            <a href="{{ route("staff.leave-approvals.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/leave-approvals") || request()->is("staff/leave-approvals/*") ? "c-active" : "" }}">
-                                <i class="fa-fw far fa-calendar-check c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.leaveApproval.title') }}
-                            </a>
-                        </li>
-                    @endcan
-                    @can('billing_access')
-                        <li class="c-sidebar-nav-dropdown {{ request()->is("staff/billing-runs*") ? "c-show" : "" }} {{ request()->is("staff/invoices*") ? "c-show" : "" }}">
-                            <a class="c-sidebar-nav-dropdown-toggle" href="#">
-                                <i class="fa-fw fas fa-hand-holding-usd c-sidebar-nav-icon">
-
-                                </i>
-                                {{ trans('cruds.billing.title') }}
-                            </a>
-                            <ul class="c-sidebar-nav-dropdown-items">
-                                @can('billing_run_access')
-                                    <li class="c-sidebar-nav-item">
-                                        <a href="{{ route("staff.billing-runs.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/billing-runs") || request()->is("staff/billing-runs/*") ? "c-active" : "" }}">
-                                            <i class="fa-fw fas fa-hand-holding-usd c-sidebar-nav-icon">
-
-                                            </i>
-                                            {{ trans('cruds.billingRun.title') }}
-                                        </a>
-                                    </li>
-                                @endcan
-                                @can('invoice_access')
-                                    <li class="c-sidebar-nav-item">
-                                        <a href="{{ route("staff.invoices.index") }}" class="c-sidebar-nav-link {{ request()->is("staff/invoices") || request()->is("staff/invoices/*") ? "c-active" : "" }}">
-                                            <i class="fa-fw fas fa-cogs c-sidebar-nav-icon">
-
-                                            </i>
-                                            {{ trans('cruds.invoice.title') }}
-                                        </a>
-                                    </li>
-                                @endcan
-                            </ul>
                         </li>
                     @endcan
                 </ul>
--- routes/web.php
+++ routes/web.php
@@ -212,14 +212,6 @@ Route::group(['as' => 'frontend.', 'namespace' => 'Frontend', 'middleware' => ['
 
 Route::group(['prefix' => 'staff', 'as' => 'staff.', 'namespace' => 'Staff', 'middleware' => ['auth']], function () {
     Route::get('/', 'HomeController@index')->name('home');
-    // Permissions
-    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
-    Route::resource('permissions', 'PermissionsController');
-
-    // Roles
-    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
-    Route::resource('roles', 'RolesController');
-
     // Users
     Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
     Route::resource('users', 'UsersController');
@@ -232,39 +224,11 @@ Route::group(['prefix' => 'staff', 'as' => 'staff.', 'namespace' => 'Staff', 'mi
     Route::delete('crm-customers/destroy', 'CrmCustomerController@massDestroy')->name('crm-customers.massDestroy');
     Route::resource('crm-customers', 'CrmCustomerController');
 
-    // Crm Note
-    Route::delete('crm-notes/destroy', 'CrmNoteController@massDestroy')->name('crm-notes.massDestroy');
-    Route::resource('crm-notes', 'CrmNoteController');
-
-    // Crm Document
-    Route::delete('crm-documents/destroy', 'CrmDocumentController@massDestroy')->name('crm-documents.massDestroy');
-    Route::post('crm-documents/media', 'CrmDocumentController@storeMedia')->name('crm-documents.storeMedia');
-    Route::post('crm-documents/ckmedia', 'CrmDocumentController@storeCKEditorImages')->name('crm-documents.storeCKEditorImages');
-    Route::resource('crm-documents', 'CrmDocumentController');
-
-    // Audit Logs
-    Route::resource('audit-logs', 'AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
-
-    // User Alerts
-    Route::delete('user-alerts/destroy', 'UserAlertsController@massDestroy')->name('user-alerts.massDestroy');
-    Route::get('user-alerts/read', 'UserAlertsController@read');
-    Route::resource('user-alerts', 'UserAlertsController', ['except' => ['edit', 'update']]);
-
     // Appointment
     Route::delete('appointments/destroy', 'AppointmentController@massDestroy')->name('appointments.massDestroy');
     Route::post('appointments/media', 'AppointmentController@storeMedia')->name('appointments.storeMedia');
     Route::post('appointments/ckmedia', 'AppointmentController@storeCKEditorImages')->name('appointments.storeCKEditorImages');
     Route::resource('appointments', 'AppointmentController');
-
-    // Photos
-    Route::delete('photos/destroy', 'PhotosController@massDestroy')->name('photos.massDestroy');
-    Route::post('photos/media', 'PhotosController@storeMedia')->name('photos.storeMedia');
-    Route::post('photos/ckmedia', 'PhotosController@storeCKEditorImages')->name('photos.storeCKEditorImages');
-    Route::resource('photos', 'PhotosController');
-
-    // Appoimntment Status
-    Route::delete('appoimntment-statuses/destroy', 'AppoimntmentStatusController@massDestroy')->name('appoimntment-statuses.massDestroy');
-    Route::resource('appoimntment-statuses', 'AppoimntmentStatusController');
 
     // Client Tags
     Route::delete('client-tags/destroy', 'ClientTagsController@massDestroy')->name('client-tags.massDestroy');
@@ -278,43 +242,7 @@ Route::group(['prefix' => 'staff', 'as' => 'staff.', 'namespace' => 'Staff', 'mi
     Route::delete('staff-availabilities/destroy', 'StaffAvailabilityController@massDestroy')->name('staff-availabilities.massDestroy');
     Route::resource('staff-availabilities', 'StaffAvailabilityController');
 
-    // Expenses
-    Route::delete('expenses/destroy', 'ExpensesController@massDestroy')->name('expenses.massDestroy');
-    Route::post('expenses/media', 'ExpensesController@storeMedia')->name('expenses.storeMedia');
-    Route::post('expenses/ckmedia', 'ExpensesController@storeCKEditorImages')->name('expenses.storeCKEditorImages');
-    Route::resource('expenses', 'ExpensesController');
-
-    // Billing Run
-    Route::delete('billing-runs/destroy', 'BillingRunController@massDestroy')->name('billing-runs.massDestroy');
-    Route::post('billing-runs/multiple-invoice-approval', 'BillingRunController@multipleInvoiceApproval')->name('billing-runs.multipleInvoiceApproval');
-    Route::put('billing-runs/generat-invoice/{id}', 'BillingRunController@generateInvoice')->name('billing-runs.generateInvoice');
-    Route::resource('billing-runs', 'BillingRunController');
-
-    // Leave Approval
-    Route::delete('leave-approvals/destroy', 'LeaveApprovalController@massDestroy')->name('leave-approvals.massDestroy');
-    Route::get('leave-approvals', 'LeaveApprovalController@index')->name('leave-approvals.index');
-    Route::get('leave-approvals/{id}', 'LeaveApprovalController@show')->name('leave-approvals.show');
-    Route::get('leave-approvals/edit/{id}', 'LeaveApprovalController@edit')->name('leave-approvals.edit');
-    Route::put('leave-approvals/approved/{id}', 'LeaveApprovalController@approved')->name('leave-approvals.approved');
-    Route::put('leave-approvals/update/{id}', 'LeaveApprovalController@update')->name('leave-approvals.update');
-
-    // Invoices
-    Route::put('invoices/generate-invoice/{id}', 'InvoicesController@generateInvoice')->name('invoices.generateInvoice')->middleware('XeroAuthenticated');
-    Route::post('invoices/generate-single-invoice', 'InvoicesController@generateSingleInvoice')->name('invoices.generateSingleInvoice');
-    Route::delete('invoices/destroy', 'InvoicesController@massDestroy')->name('invoices.massDestroy')->middleware('XeroAuthenticated');
-    Route::resource('invoices', 'InvoicesController')->middleware('XeroAuthenticated');
-
-    Route::get('system-calendar', 'SystemCalendarController@index')->name('systemCalendar');
     Route::get('global-search', 'GlobalSearchController@search')->name('globalSearch');
-    Route::get('messenger', 'MessengerController@index')->name('messenger.index');
-    Route::get('messenger/create', 'MessengerController@createTopic')->name('messenger.createTopic');
-    Route::post('messenger', 'MessengerController@storeTopic')->name('messenger.storeTopic');
-    Route::get('messenger/inbox', 'MessengerController@showInbox')->name('messenger.showInbox');
-    Route::get('messenger/outbox', 'MessengerController@showOutbox')->name('messenger.showOutbox');
-    Route::get('messenger/{topic}', 'MessengerController@showMessages')->name('messenger.showMessages');
-    Route::delete('messenger/{topic}', 'MessengerController@destroyTopic')->name('messenger.destroyTopic');
-    Route::post('messenger/{topic}/reply', 'MessengerController@replyToTopic')->name('messenger.reply');
-    Route::get('messenger/{topic}/reply', 'MessengerController@showReply')->name('messenger.showReply');
 
 });
 
